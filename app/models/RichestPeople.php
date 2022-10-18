3ab5e91044dffc353ac027ec30d3d82d23095f4e
NBSKATE <niko.baltzis@gmail.com>
2022-10-18 09:21:36 +0200

    Initial commit 4

--- app/models/RichestPeople.php
+++ app/models/RichestPeople.php
@@ -1,50 +1,50 @@
 <?php
 
 /**
- * Dit is de model voor de controller Countries
+ * Dit is de model voor de controller RichestPeoples
  */
 
-class Country
+class RichestPeople
 {
-    // Dit is de constructor van de Country model class
+    // Dit is de constructor van de RichestPeople model class
     public function __construct()
     {
         $this->db = new Database();
     }
 
-    public function getCountries()
+    public function getPeople()
     {
-        $this->db->query('SELECT * FROM Country');
+        $this->db->query('SELECT * FROM richestpeople');
         return $this->db->resultSet();
     }
 
-    public function getCountry($id)
+    public function getPeople($id)
     {
-        $this->db->query("SELECT * FROM country WHERE id = :id");
+        $this->db->query("SELECT * FROM richestpeople WHERE id = :id");
         $this->db->bind(':id', $id, PDO::PARAM_INT);
         return $this->db->single();
     }
 
-    public function updateCountry($data)
+    public function updatePeople($data)
     {
         var_dump($data);
-        $this->db->query("UPDATE Country
+        $this->db->query("UPDATE RichestPeople
                           Set Name = :Name,
-                          CapitalCity = :CapitalCity,
-                          Continent = :Continent,
-                          Population = :Population
+                          Networth = :Networth,
+                          MyAge = :MyAge,
+                          Company = :Company
                         WHERE id = :id");
 
         $this->db->bind(':Name', $data['name'], PDO::PARAM_STR);
-        $this->db->bind(':CapitalCity', $data['capitalCity'], PDO::PARAM_STR);
-        $this->db->bind(':Continent', $data['continent'], PDO::PARAM_STR);
-        $this->db->bind(':Population', $data['population'], PDO::PARAM_STR);
+        $this->db->bind(':Networth', $data['networth'], PDO::PARAM_STR);
+        $this->db->bind(':MyAge', $data['myage'], PDO::PARAM_STR);
+        $this->db->bind(':Company', $data['company'], PDO::PARAM_STR);
         $this->db->bind(':id', $data['id'], PDO::PARAM_INT);
 
         return $this->db->execute();
     }
 
-    public function deleteCountry($id)
+    public function deletePeople($id)
     {
         $this->db->query("DELETE FROM country WHERE id = :id");
         $this->db->bind(':id', $id, PDO::PARAM_INT);
